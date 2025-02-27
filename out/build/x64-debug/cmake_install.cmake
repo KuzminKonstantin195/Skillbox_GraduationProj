05a031b522f7f19dd81812a1855c3560854f4f1e
Konstantin Kuzmin <konstantinkuzmin195@gmail.com>
2025-02-27 11:16:03 +0700

    add gtest and gitignore

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -32,6 +32,13 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/konst/OneDrive/Рабочий стол/Development/Skillbox_GraduationProj/out/build/x64-debug/search_engine/cmake_install.cmake")
+  include("C:/Users/konst/OneDrive/Рабочий стол/Development/Skillbox_GraduationProj/out/build/x64-debug/tests/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
---
+++ search_engine/sources/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+//#include "nlohmann/json.hpp"
+
+using namespace std;
+
+int main()
+{
+}
