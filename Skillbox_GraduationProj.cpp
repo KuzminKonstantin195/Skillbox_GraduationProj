c2129a8544fc30cab01c7a10719597aae02ec3de
Konstantin Kuzmin <konstantinkuzmin195@gmail.com>
2025-02-27 09:15:01 +0700

    init

---
+++ Skillbox_GraduationProj.cpp
@@ -0,0 +1,12 @@
+﻿// Skillbox_GraduationProj.cpp: определяет точку входа для приложения.
+//
+
+#include "Skillbox_GraduationProj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
